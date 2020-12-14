e8c6e8ed523ebda3eea028a3d57267efc0051104
Senthil kumar K <nightrider.cbe@gmail.com>
2020-12-14 10:16:12 +0530

    Commit

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  puzzle
+//
+//  Created by Senthil Kumar2 on 14/12/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let str : String = multiflyWords(inputStr: "Hello", repeat: 5)
+        print(str)
+        
+    }
+
+    
+    func multiflyWords(inputStr:String,repeat:Int) -> String {
+        
+        var tempArr : [String] = []
+        var finalStr :String = ""
+        
+        for  _ in 1...`repeat`{
+            
+            tempArr.append(inputStr)
+        }
+        
+        finalStr = tempArr.joined(separator: " ")
+        
+        return finalStr
+    }
+
+}
+
